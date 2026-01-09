50ceb7a57b5642552639a8df43467ac65fd18003
polina3 <e.supranpolina@gmail.com>
2026-01-09 22:32:17 +0300

    fix: update Yandex OAuth redirect URI to correct production URL

--- server/constants/yandex.ts
+++ server/constants/yandex.ts
@@ -5,7 +5,7 @@
 export const YANDEX_CONFIG = {
     CLIENT_ID: process.env.YANDEX_CLIENT_ID || '',// ID приложения, полученный при регистрации на developer.yandex.ru
     CLIENT_SECRET: process.env.YANDEX_CLIENT_SECRET || '',// Секретный ключ приложения 
-    REDIRECT_URI: process.env.YANDEX_REDIRECT_URI || 'http://93.189.231.213:3000/api/auth/yandex/callback', // URL перенаправления после авторизации
+    REDIRECT_URI: process.env.YANDEX_REDIRECT_URI || 'http://85.198.87.33:3000/api/auth/yandex/callback', // URL перенаправления после авторизации
     AUTH_URL: 'https://oauth.yandex.ru/authorize', // URL для начала OAuth процесса авторизации
     TOKEN_URL: 'https://oauth.yandex.ru/token', // URL для обмена кода авторизации на access token
     USER_INFO_URL: 'https://login.yandex.ru/info' // URL для получения информации о пользователе
